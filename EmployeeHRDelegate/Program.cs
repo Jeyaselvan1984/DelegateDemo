d89a8fa25f0f158f1fc6358a307ccc80360b9593
Jeyaselvan <jeyaselvan.navaraj@gmail.com>
2020-02-09 23:30:39 -0500

    Add project files.

---
+++ EmployeeHRDelegate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace EmployeeHRDelegate
+{
+    public delegate void NewEmployeeEventHandler(object sender, NewEmployeeEventArgs e);
+
+    public class NewEmployeeEventArgs : EventArgs
+    {
+        public string Name;
+        public int Age;
+        public NewEmployeeEventArgs(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    class HR
+    {
+        public event NewEmployeeEventHandler NewEmployee;
+
+        public void OnNewEmployee(NewEmployeeEventArgs e)
+        {
+            if (NewEmployee != null)
+                NewEmployee(this, e);
+        }
+
+        public void RegisterEmployee(string name, int age)
+        {
+            NewEmployeeEventArgs e = new NewEmployeeEventArgs(name, age);
+            OnNewEmployee(e);
+        }
+    }
+
+    class EmployeeCare
+    {
+        public EmployeeCare(HR hr)
+        {
+            hr.NewEmployee += CallEmployee;
+        }
+
+        private void CallEmployee(object sender, NewEmployeeEventArgs e)
+        {
+            Console.WriteLine("Sender of event: " + sender.ToString());
+            Console.WriteLine("Cusomer Info: " + e.Name + e.Age.ToString());
+            //do call Employee stuff
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HR hr = new HR(); //line1
+            EmployeeCare cc = new EmployeeCare(hr); //line2
+            hr.RegisterEmployee("mohamad", 26); //line3
+            Console.ReadLine(); //line4
+        }
+    }
+}
